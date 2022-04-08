70f5373f06258bdbcb12552ded1bb1dd515ca02e
silasmas <silasjmas@gmail.com>
2022-04-08 03:59:13 +0200

    authentification, reinitialisation et inscription fini

--- resources/views/client/pages/loginnew.blade.php
+++ resources/views/client/pages/loginnew.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ config('app.name') }}  Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/mystyle.css') }}" />
@@ -29,27 +29,44 @@
                         </a>
                         <h2 class="mb-5">Bienvenue à Cado</h2>
                         <h1>Connexion</h1>
-                        <form action="">
+                        
+                        <div class="col-md-12  text-danger mb-5">
+                            @foreach ($errors->all() as $err)
+                                {{$err}}
+                            @endforeach
+                            {{ session('status') }}
+                        </div>
+                        <form  method="POST" action="{{ route('login') }}">
+                            @csrf
                             <div class="form-group row g-5 g-lg-5">
                                 <div class="col-lg-12">
-                                    <input type="email" class="form-control" placeholder="Email">
+                                    <input type="email" name="email" class="form-control" value="{{ old('email') }}" placeholder="Email"
+                                    required autofocus>
                                     <div class="icon">
                                        @
                                     </div>
                                     <div class="line"></div>
                                 </div>
                                 <div class="col-lg-12">
-                                    <input type="password" class="form-control" placeholder="Mot de passe">
+                                    <input  class="form-control" placeholder="Mot de passe" type="password"
+                                    name="password"
+                                    required autocomplete="current-password" >
                                     <div class="icon">
                                         <i class="bi bi-key"></i>
                                     </div>
                                     <div class="line"></div>
                                 </div>
-                                <div class="col-lg-12">
+                                <div class="col-lg-6">
+                                    <label for="remember_me" class="inline-flex items-center">
+                                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                                        <span class="reset">{{ __('Remember me') }}</span>
+                                    </label>
+                                </div>
+                                <div class="col-lg-6">
                                    <a href="{{ route('password.request') }}" class="reset">Mot de passe oubliez ?</a>
                                 </div>
                                 <div class="col-lg-12 text-center">
-                                    <button class="btn mb-4">Se connecter</button>
+                                    <button class="btn mb-4" type="submit">Se connecter</button>
                                     <p>Ou</p>
                                     <a href="{{ route('registerUser') }}" class="reset">S'inscrire</a>
                                 </div>
