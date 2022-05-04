25620733f3dfcebafac258b4b85f7f973b4d2c49
silasmas <silasjmas@gmail.com>
2022-05-04 03:23:59 +0200

    cours

--- resources/views/client/pages/mesCours.blade.php
+++ resources/views/client/pages/mesCours.blade.php
@@ -37,8 +37,8 @@
             </div>
         </div>
         <div class="row no-gutters" id="my_courses_area">*
- 
-            @forelse ($userForm->session as $fr)
+ @if (is_array($userForm->session) || is_object($userForm->session))
+      @forelse ($userForm->session as $fr)
                 <div class="col-lg-3">
                 <div class="course-box-wrap">
                     <div class="course-box">
@@ -114,6 +114,8 @@
             @empty
                 
             @endforelse
+ @endif
+           
           
 
            
