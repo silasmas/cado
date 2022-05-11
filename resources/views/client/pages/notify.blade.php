4970917f7489b1a6830ca4fd5e4db4e098e739e8
silasmas <silasjmas@gmail.com>
2022-05-11 06:46:55 +0200

    dd

--- resources/views/client/pages/notify.blade.php
+++ resources/views/client/pages/notify.blade.php
@@ -16,7 +16,7 @@
 
 <body class="antialiased">
 
-    {{ dd($data) }}
+    
     <div class="container">
         <div class="alert {{ $data['data']['status']=="ACCEPTED"?"alert-success":"alert-danger" }} " role="alert">
        
