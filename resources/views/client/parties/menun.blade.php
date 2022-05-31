ce0ecf0a9ca0e7346c3a7250fc1bb72b3d39e877
silasmas <silasjmas@gmail.com>
2022-05-31 17:35:58 +0200

    efe

--- resources/views/client/parties/menun.blade.php
+++ resources/views/client/parties/menun.blade.php
@@ -59,7 +59,8 @@
                     {{-- debut menu live --}}
                     <div class="instructor-box  menu-icon-box" id="">
                         <div class="icon">
-                            <a href="{{ $live->count() > 0 ? route('listelives') : route('hone') }}"><i
+                            <a href="{{ $live->count() > 0 ? route('listelives') : route('home') }}"><i
+                                
                                     class="fas fa-signal"></i>
                             </a>
                             <span class="number">{{ $live->count() }}</span>
