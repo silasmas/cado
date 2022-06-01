e3f51dc6f9353e2b7f5ad01f332fe6ab154ebd10
silasmas <silasjmas@gmail.com>
2022-06-01 19:13:34 +0200

    mail

--- resources/views/emails/achat.blade.php
+++ resources/views/emails/achat.blade.php
@@ -1,12 +1,12 @@
 @component('mail::message')
 # Félicitation {{$user->prenom.' '.$user->name}}
 
-{{ $data['message'] }}
 
 @component('mail::panel')
-Bénéficiez des nos coachings dans diverse domaine.<br>
-Cliquez sur le boutton ci-dessous pour revenir sur le site.
+{{ $data['message'] }}
 @endcomponent
+{{ "Bénéficiez des nos coachings dans diverse domaine.".<br>."
+Cliquez sur le boutton ci-dessous pour revenir sur le site." }}
 <br>
 @component('mail::button', ['url' => config('app.url')])
 Retour sur le site {{config('app.name')}}
