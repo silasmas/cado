ded7301a693fa3928e0cd31abdc1b8b287434e81
silasmas <silasjmas@gmail.com>
2022-06-01 19:18:24 +0200

    ff

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
+@if (trim($slot) === 'A&N')
 <img src="{{ asset('assets/logo/logoan.png') }}" class="logo" alt="A&N">
 @else
 {{ $slot }}
