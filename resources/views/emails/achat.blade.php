cf135a3560e1ca69550eddb2a0668bcdaaa5b249
silasmas <silasjmas@gmail.com>
2022-06-04 18:41:12 +0200

    s

--- resources/views/emails/achat.blade.php
+++ resources/views/emails/achat.blade.php
@@ -1,24 +1,22 @@
 @component('mail::message')
     # {{ $type == 'success' ? 'Félicitation ' : 'Bonjjour ' }} {{ $user->prenom . ' ' . $user->name }}
-    @if ($type == 'success')
-        @component('mail::panel')
+    @component('mail::panel')
+        @if ($type == 'success')
             @if ($session->live == true && $session->isform == false)
                 {{ 'La réservation du live ' }}<b>{{ $session->titre }}</b>{{ ' est faite avec succès. Verfifiez votre compte pour plus de details' }}
             @else
                 {{ "L'achat de la Formation " }}<b>{{ $session->titre }}</b>
                 {{ ' à réussit. Verfifiez votre compte pour le suivre.' }}
             @endif
-        @endcomponent
-    @else
-        @component('mail::panel')
+        @else
             @if ($session->live == true && $session->isform == false)
                 {{ 'La réservation du live ' }}<b>{{ $session->titre }}</b>{{ ' a échoué. Verfifiez votre compte pour plus de details' }}
             @else
                 {{ "L'achat de la Formation " }}<b>{{ $session->titre }}</b>
                 {{ ' à échoué. Verfifiez votre compte pour plus de details.' }}
             @endif
-        @endcomponent
-    @endif
+        @endif
+    @endcomponent
 
     <p>
         Bénéficiez des nos coachings dans diverse domaine.
